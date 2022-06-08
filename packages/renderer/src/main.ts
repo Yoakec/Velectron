a273bfcdccbb138cc9aa96939da59526a024bfa1
Yoakec <1602491057@qq.com>
2022-06-08 15:41:16 +0800

    auto import unocss

--- packages/renderer/src/main.ts
+++ packages/renderer/src/main.ts
@@ -1,6 +1,16 @@
+/*
+ * @Author: Yoakec 1602491057@qq.com
+ * @Date: 2022-06-08 00:25:08
+ * @LastEditors: Yoakec 1602491057@qq.com
+ * @LastEditTime: 2022-06-08 14:12:40
+ * @FilePath: \myelectron\packages\renderer\src\main.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { createApp } from 'vue'
 import App from './App.vue'
 import './samples/node-api'
+
+import 'uno.css'
 
 createApp(App)
   .mount('#app')
--- packages/renderer/vite.config.ts
+++ packages/renderer/vite.config.ts
@@ -1,16 +1,59 @@
+/*
+ * @Author: Yoakec 1602491057@qq.com
+ * @Date: 2022-06-08 00:25:08
+ * @LastEditors: Yoakec 1602491057@qq.com
+ * @LastEditTime: 2022-06-08 14:57:43
+ * @FilePath: \myelectron\packages\renderer\vite.config.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { defineConfig } from 'vite'
+import path from 'path'
 import vue from '@vitejs/plugin-vue'
 import resolve, { lib2esm } from 'vite-plugin-resolve'
 import electron from 'vite-plugin-electron/renderer'
 import pkg from '../../package.json'
 
+import Unocss from 'unocss/vite'
+import { presetUno, presetAttributify, presetIcons } from 'unocss'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   mode: process.env.NODE_ENV,
   root: __dirname,
+  resolve:{
+    alias: {
+      '~/': `${path.resolve(__dirname, 'src')}/`,
+    }
+  },
   plugins: [
     vue(),
     electron(),
+    Unocss({
+      presets: [
+          presetUno(), 
+          presetAttributify(), 
+          presetIcons()],
+    }),
+    AutoImport({
+      imports: [
+        'vue',
+        // 'vue/macros',
+        // 'vue-router',
+        '@vueuse/core',
+      ],
+      dts:'src/auto-import.d.ts',
+    }),
+    Components({
+      // 指定组件位置，默认是src/components
+      dirs: ['src/components'],
+      // ui库解析器
+      // resolvers: [ElementPlusResolver()],
+      extensions: ['vue'],
+      // 配置文件生成位置
+      dts: 'src/components.d.ts'
+    }),
     resolve(
       /**
        * Here you can specify other modules
